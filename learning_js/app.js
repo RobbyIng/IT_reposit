03924206736c16db7a64f86c51ed8c743876b027
Robert Bakirov <bakirovrm@yandex.ru>
2022-11-12 21:01:38 +0400

    Add check speed function

--- learning_js/app.js
+++ learning_js/app.js
@@ -1,4 +1,21 @@
-
-for (let i = 0; i < 5; i++){
-console.log("Hello World!");
+function max(a,b) {
+    return (a > b) ? a : b;
 }
+function isLanscape(width, height){
+    return (width > height);
+}
+function fizzBazz (input) {
+ if (typeof(input) !== "number") return "Not a number";
+ if ((input % 3 === 0) &&  (input % 5 ===0)) return "FizzBazz"; 
+ if (input % 3 ===0) return "Fizz";
+ if (input % 5 ===0) return "Bazz"; 
+ return input;
+}
+function checkSpeed (actSpeed){
+    const speedLimit = 70;
+    const speedPerPoint = 5;
+if (actSpeed < speedLimit + speedPerPoint) return "OK";
+    let f = Math.floor((actSpeed - speedLimit) / speedPerPoint);
+    return (f < 12) ? f : "Licence suspended";
+}
+console.log(checkSpeed(130));
