1c3faa8adf20b9c8a3b3d9788be7922f81e56395
Robert Bakirov <bakirovrm@yandex.ru>
2022-12-01 20:30:19 +0400

    after third exercise

--- learning_js/objects/script.js
+++ learning_js/objects/script.js
@@ -1,11 +1,50 @@
-const address = {
-    street: "Gagarina",
-    city: "Ulyanovsk",
-    zipCode: 432071
+// const address = {
+//     street: "Gagarina",
+//     city: "Ulyanovsk",
+//     zipCode: 432071
+// }
+// function showAddress (address){
+//     for (let key in address){
+//         console.log(key,address[key])
+//     }
+// }
+// showAddress(address);
+
+//exercise 2
+
+// let address =  addAddress("Gagarina", "Ulyanovsk", 432071);
+// function addAddress(street,city,zipCode) {
+//     return{
+//     street,
+//     city,
+//     zipCode   
+//     };
+// }
+// console.log(address);
+// function AddAddress(street,city,zipCode) {
+//     this.street = street,
+//     this.city = city,
+//     this.zipCode = zipCode
+// }
+
+// const address2 = new AddAddress("Gagarina", "Ulyanovsk", 432071);
+// console.log(address2);
+
+//exercise 3
+let address1 = new AddAddress("Gagarina", "Ulyanovsk", 432071);
+let address2 = new AddAddress("Gagarina", "Ulyanovsk", 432071);
+function AddAddress(street,city,zipCode) {
+    this.street = street,
+    this.city = city,
+    this.zipCode = zipCode
 }
-function showAddress (address){
-    for (key in address){
-        console.log(key,address[key])
-    }
+function areEqual(address1,address2) {
+for (let key in address1){
+  if (address1[key] !== address2[key]) return false  
 }
-showAddress(address);
+return true;
+}
+function areSame(address1,address2) {
+    return address1 === address2;
+}
+console.log(areEqual(address1,address2));
