99ddfac5d6172f8a4b11e9bb6badea78639cd519
Robert Bakirov <bakirovrm@yandex.ru>
2022-12-04 17:04:29 +0400

    changed 9.17

--- Sber_univer/9.17/script.js
+++ Sber_univer/9.17/script.js
@@ -7,19 +7,29 @@ const obj = {
 };
 const obj2 = {};
 
-for (const key of Object.keys(obj)) {
-    let count = 0;    
-    let isThere = false; 
-        for (const keys of Object.keys(obj2)) {
-            if (obj[key] === keys) {
-                isThere = true;
-                break;
-            }
-        }
-    if (isThere) {
-    obj2[obj[key]] += 1;  
-    }
-    else obj2[obj[key]] = 1;   
-}
-console.log(obj2);
+// for (const key of Object.keys(obj)) {
+//     let isThere = false; 
+//         for (const keys of Object.keys(obj2)) {
+//             if (obj[key] === keys) {
+//                 isThere = true;
+//                 break;
+//             }
+//         }
+//     if (isThere) {
+//     obj2[obj[key]] += 1;  
+//     }
+//     else obj2[obj[key]] = 1;   
+// }
+// console.log(obj2);
 
+// tutor
+
+for (let k in obj){
+    if (obj2[obj[k]]) {
+        obj2[obj[k]] ++;
+    }
+    else{
+        obj2[obj[k]] = 1;
+    }
+    }
+    console.log(obj2);
