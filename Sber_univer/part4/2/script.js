b1d386f35aa1bafc7d0f52c96bd8f3066cb5f7dd
John Doe <johndoe@email.com>
2022-12-29 18:46:30 +0400

    2.4 - 2.7

--- Sber_univer/part4/2/script.js
+++ Sber_univer/part4/2/script.js
@@ -19,10 +19,48 @@
 
 // 2.3
 
-let new_element = document.createElement("li");
-new_element.innerText = "Велосипед";
-let ulObj = document.querySelector("ul");
-let obj = document.querySelector("li");
-ulObj.insertBefore(new_element,obj);
+// let new_element = document.createElement("li");
+// new_element.innerText = "Велосипед";
+// let ulObj = document.querySelector("ul");
+// let obj = document.querySelector("li");
+// ulObj.insertBefore(new_element,obj);
 
-console.log(obj);
+// console.log(obj);
+
+// 2.4
+
+// let objects = document.querySelectorAll("div");
+// console.log(objects);
+// for (item of objects){
+//     item.className = "adds";
+// }
+// console.log(objects);
+
+// 2.5
+
+// let objects = document.querySelectorAll("p");
+// console.log(objects);
+// let i = 0;
+// for (item of objects){
+//     i++
+//     item.id = `p_${i}`;
+// }
+// console.log(objects);
+
+// 2.6
+// let objects = document.querySelectorAll("p");
+// console.log(objects);
+
+//  for (item of objects){
+//     if (item.innerText === "Delete")  item.remove();
+// }
+// console.log(objects);
+
+// 2.7
+
+// const values=[15, 20, 25];
+// const objects = document.getElementsByClassName("elems");
+
+// for (let i = objects.length-1; i >= 0; i--){
+//     if (values.includes(+objects[i].innerText))  {objects[i].remove()};
+// }
